4151096cd457c9fd1693b3374fb88f177a4cf81f
Ragini singh <singhragni789@gmail.com>
2025-01-26 11:47:08 +0530

    add datatype

---
+++ 01_var.js
@@ -0,0 +1,21 @@
+const accountId = 18999;
+let emailId = "singhragni789@gamil.com";
+
+// accountId = 90  // not allowed
+var accountEmail = "singhragni@thinksys.com";
+
+accountEmail = "singhTest@gmial";
+emailId = "test5@gmail.com";
+
+let accountState;
+
+
+
+/**
+ * do not use var . 
+ * only use let const .
+ */
+
+console.log(accountState);
+
+console.table([accountId, accountEmail,emailId,accountState]);
---
+++ 02_DataType.js
@@ -0,0 +1,13 @@
+"Use Strict" // Use for strit mode.
+
+// number
+// string = " ",
+// bigInt = ,
+// null ,
+// underfined there is no value 
+// symbole - when we are taling about unique value.
+// type of
+
+
+console.log(typeof null) // object
+console.log(typeof undefined); // undefined
