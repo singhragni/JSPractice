a924622cbed2083838e24fbe6a3918f4c89e5279
Ragini singh <singhragni789@gmail.com>
2025-01-26 14:48:01 +0530

    add stack and heap concept

---
+++ 0_4StackandHeap.js
@@ -0,0 +1,18 @@
+let myTest = 123;
+let myTest2  =myTest;
+console.log(myTest);
+myTest2 = 1900;
+console.log(myTest2);
+
+let myObject = {
+    x :1,
+    y:3
+}
+
+
+
+let mySecObject =  myObject;
+
+mySecObject.x = 9
+console.log(mySecObject.x);
+console.log(myObject.x);
